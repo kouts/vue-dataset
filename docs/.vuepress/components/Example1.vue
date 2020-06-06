622b125c2b36e3d14eba3eb7c6d653360a142b13
E6440\Kouts <koutsaftakis@gmail.com>
2020-06-06 22:20:47 +0300

    Added example data

--- docs/.vuepress/components/Example1.vue
+++ docs/.vuepress/components/Example1.vue
@@ -2,18 +2,21 @@
   <div class="mt-3">
     <div class="row mb-3">
       <div class="col">
-        This is the first {{ name }}!
+        {{ users }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// https://next.json-generator.com/4JvxrAE2O
+import data from '../data/exampleData.json';
+
 export default {
   name: 'Example1',
   data: function () {
     return {
-      name: 'example updated test!'
+      users: data
     };
   }
 };
