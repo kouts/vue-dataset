95350361ba5d27d2a9b98f2b52aaf7be92442f30
kouts <koutsaftakis@gmail.com>
2020-06-13 20:21:29 +0300

    Fixed results number check to be of strict equality

--- src/DatasetInfo.vue
+++ src/DatasetInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    {{ datasetI18n.showing }} {{ dsResultsNumber != 0 ? dsFrom + 1 : 0 }}
+    {{ datasetI18n.showing }} {{ dsResultsNumber !== 0 ? dsFrom + 1 : 0 }}
     {{ datasetI18n.showingTo }} {{ dsTo >= dsResultsNumber ? dsResultsNumber : dsTo }}
     {{ datasetI18n.showingOf }} {{ dsResultsNumber }} {{ datasetI18n.showingEntries }}
   </div>
