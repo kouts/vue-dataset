de2e944dec6f56fcaebbc2b51821ed5a092767eb
E6440\Kouts <koutsaftakis@gmail.com>
2020-06-13 12:26:26 +0300

    Added test for injected search method

--- tests/unit/DatasetSearch.spec.js
+++ tests/unit/DatasetSearch.spec.js
@@ -1,15 +1,29 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createWrapper } from '@vue/test-utils';
 import DatasetSearch from '@/DatasetSearch.vue';
 
 describe('DatasetSearch', () => {
+  const testDiv = document.createElement('div');
+  testDiv.setAttribute('id', 'test-div');
+  document.body.appendChild(testDiv);
+
   const wrapper = shallowMount(DatasetSearch, {
     provide: {
       search: function (value) {
-        console.log(value);
+        const testDiv = document.getElementById('test-div');
+        testDiv.textContent = value;
       }
     }
   });
+
   it('renders an input element', () => {
-    expect(wrapper.findAll('.form-control').length).toEqual(1);
+    const inputText = wrapper.find('input.form-control');
+    expect(inputText.exists()).toBe(true);
+  });
+
+  it('passes the correct value to the injected search method', () => {
+    const inputText = wrapper.find('input.form-control');
+    inputText.setValue('test');
+    const testDiv = document.getElementById('test-div');
+    expect(testDiv.textContent).toEqual('test');
   });
 });
