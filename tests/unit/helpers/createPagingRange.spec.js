36caf978f990360527de37f5fc38ac830ecf6a42
E6440\Kouts <koutsaftakis@gmail.com>
2020-06-27 23:46:24 +0300

    Added tests for createPagingRange

--- tests/unit/helpers/createPagingRange.spec.js
+++ tests/unit/helpers/createPagingRange.spec.js
@@ -6,6 +6,22 @@ describe('createPagingRange', () => {
     const res = createPagingRange(3, 1);
     expect(res).toStrictEqual([1, 2, 3]);
   });
+  it('should return the correct range for 10 pages when current page is 9', () => {
+    const res = createPagingRange(10, 9);
+    expect(res).toStrictEqual([1, '...', 7, 8, 9, 10]);
+  });
+  it('should return the correct range for 20 pages when current page is 3', () => {
+    const res = createPagingRange(20, 3);
+    expect(res).toStrictEqual([1, 2, 3, 4, 5, '...', 20]);
+  });  
+  it('should return the correct range for 10 pages when current page is 10', () => {
+    const res = createPagingRange(10, 10);
+    expect(res).toStrictEqual([1, '...', 8, 9, 10]);
+  });
+  it('should return the correct range for 100 pages when current page is 5', () => {
+    const res = createPagingRange(100, 5);
+    expect(res).toStrictEqual([1, 2, 3, 4, 5, 6, 7, '...', 100]);
+  });
   it('should return the correct range for 100 pages when current page is 1', () => {
     const res = createPagingRange(100, 1);
     expect(res).toStrictEqual([1, 2, 3, '...', 100]);
