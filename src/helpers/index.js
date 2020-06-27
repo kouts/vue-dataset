0c3edd7f01bb6ed08838364121f4df4c79e39868
E6440\Kouts <koutsaftakis@gmail.com>
2020-06-27 13:24:16 +0300

    Created separate file for helper methods

---
+++ src/helpers/index.js
@@ -0,0 +1,127 @@
+function isEmptyObject (obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function createPagingRange (nrOfPages, currentPage) {
+  const delta = 2;
+  const range = [];
+  const rangeWithDots = [];
+  let l;
+
+  range.push(1);
+
+  if (nrOfPages <= 1) {
+    return range;
+  }
+
+  for (let i = currentPage - delta; i <= currentPage + delta; i++) {
+    if (i < nrOfPages && i > 1) {
+      range.push(i);
+    }
+  }
+  range.push(nrOfPages);
+
+  for (let j = 0; j < range.length; j++) {
+    if (l) {
+      if (range[j] - l === 2) {
+        rangeWithDots.push(l + 1);
+      } else if (range[j] - l !== 1) {
+        rangeWithDots.push('...');
+      }
+    }
+    rangeWithDots.push(range[j]);
+    l = range[j];
+  }
+  return rangeWithDots;
+}
+
+function fieldSorter (fields) {
+  const dir = [];
+  let i;
+  const l = fields.length;
+  fields = fields.map(function (o, i) {
+    if (o[0] === '-') {
+      dir[i] = -1;
+      o = o.substring(1);
+    } else {
+      dir[i] = 1;
+    }
+    return o;
+  });
+
+  return function (a, b) {
+    for (i = 0; i < l; i++) {
+      const o = fields[i];
+      if (a.value[o] > b.value[o]) {
+        return dir[i];
+      }
+      if (a.value[o] < b.value[o]) {
+        return -(dir[i]);
+      }
+    }
+    return 0;
+  };
+}
+
+function fieldFilter (result, filterFields) {
+  // Filter it by field
+  for (const filterKey in filterFields) {
+    // console.log(filterKey + ' -> ' + filterFields[filterKey]);
+    result = result.filter(function (entry) {
+      const entryValue = entry.value;
+      for (const entryKey in entryValue) {
+        if (entryKey === filterKey) {
+          if (typeof filterFields[filterKey] === 'function') {
+            return filterFields[filterKey](entryValue[entryKey]);
+          }
+          if (filterFields[filterKey] === '') {
+            return true;
+          }
+          if (entryValue[entryKey] === filterFields[filterKey]) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
+  }
+  return result;
+}
+
+// Search method that also takes into account transformations needed
+function findAny (dsSearchIn, dsSearchAs, obj, str) {
+  // Convert the search string to lower case
+  str = str.toLowerCase();
+  for (const key in obj) {
+    if (dsSearchIn.length === 0 || dsSearchIn.indexOf(key) !== -1) {
+      const value = String(obj[key]).toLowerCase();
+      for (const field in dsSearchAs) {
+        if (field === key) {
+          // Found key in dsSearchAs so we pass the value and the search string to a search function
+          // that returns true/false and we return that if true.
+          /* Check if dsSearchAs was passed as string from template, if so call appropriate function from the component */
+          if (typeof dsSearchAs[field] === 'string') {
+            const res = this[dsSearchAs[field]](value, str);
+            if (res === true) {
+              return res;
+            }
+            /* Check if dsSearchAs is a function (passed from the template) */
+          }
+          if (typeof dsSearchAs[field] === 'function') {
+            const res = dsSearchAs[field](value, str);
+            if (res === true) {
+              return res;
+            }
+          }
+        }
+      }
+      // If it doesn't return from above we perform a simple search
+      if (value.indexOf(str) >= 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export { isEmptyObject, createPagingRange, fieldSorter, fieldFilter, findAny };
