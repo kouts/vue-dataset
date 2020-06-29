8c24551b72aa1408bd9b70eef375307c3e199f57
kouts <koutsaftakis@gmail.com>
2020-06-29 23:40:56 +0300

    Put test data into a separate file

--- tests/unit/helpers/fieldSorter.spec.js
+++ tests/unit/helpers/fieldSorter.spec.js
@@ -1,29 +1,11 @@
 import { fieldSorter } from '@/helpers';
+import { data } from './testData.js';
 
 function isoDateToDate (isoDate) {
   const isoDateParts = isoDate.split('-').map(o => Number(o));
   const res = new Date(new Date(isoDateParts[0], isoDateParts[1] - 1, isoDateParts[2]));
   return res;
 };
-
-const data = [
-  {
-    index: 0,
-    value: { balance: 39317, birthdate: '1980-03-09', name: 'Gawain Bumpsty', firstName: 'Gawain', lastName: 'Bumpsty', company: 'Quaxo', email: 'gcumpsty0@discuz.net', phone: '985-353-3917', address: '4669 Northland Circle', favoriteAnimal: 'Deer, roe' }
-  },
-  {
-    index: 1,
-    value: { balance: 64949, birthdate: '2004-02-11', name: 'Mile Fulloway', firstName: 'Mile', lastName: 'Fulloway', company: 'Flashspan', email: 'mfulloway2@whitehouse.gov', phone: '225-853-2332', address: '683 Lakeland Crossing', favoriteAnimal: 'Starling, cape' }
-  },
-  {
-    index: 2,
-    value: { balance: 3855, birthdate: '1974-05-14', name: 'Bob Kubiak', firstName: 'Bob', lastName: 'Kubiak', company: 'Youspan', email: 'gkubiak3@tripadvisor.com', phone: '235-467-8727', address: '157 Fisk Center', favoriteAnimal: 'Fairy penguin' }
-  },
-  {
-    index: 3,
-    value: { balance: 21192, birthdate: '1972-09-29', name: 'Gawain Arraway', firstName: 'Gawain', lastName: 'Arraway', company: 'Shuffledrive', email: 'eharraway1@wordpress.com', phone: '217-861-5367', address: '1 Meadow Valley Circle', favoriteAnimal: 'African clawless otter' }
-  }
-];
 
 let sortData = [];
 
