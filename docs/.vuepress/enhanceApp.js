c4cc35249b3f903f9e84dae37d4729c4067e6ee1
E6440\Kouts <koutsaftakis@gmail.com>
2021-01-10 11:33:28 +0200

    Created source-view component

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,6 +4,7 @@ import DatasetShow from '../../src/DatasetShow.vue';
 import DatasetPager from '../../src/DatasetPager.vue';
 import DatasetItem from '../../src/DatasetItem.vue';
 import DatasetInfo from '../../src/DatasetInfo.vue';
+import SourceView from '../../src/source-view/SourceView.vue';
 import './styles/styles.scss';
 
 export default ({
@@ -14,6 +15,7 @@ export default ({
 }) => {
   if (typeof process === 'undefined') {
     // process is undefined in a browser
+    Vue.component('source-view', SourceView);
     Vue.component('dataset', Dataset);
     Vue.component('dataset-show', DatasetShow);
     Vue.component('dataset-search', DatasetSearch);
---
+++ src/source-view/SourceView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello world</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
