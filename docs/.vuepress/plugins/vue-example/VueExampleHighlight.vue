86f8f8d7bcddd58ac92852b04ef4e64f1a00dc1b
Kouts <koutsaftakis@gmail.com>
2021-02-25 21:24:47 +0200

    Removed unneeded check

--- docs/.vuepress/plugins/vue-example/VueExampleHighlight.vue
+++ docs/.vuepress/plugins/vue-example/VueExampleHighlight.vue
@@ -20,11 +20,6 @@ export default {
     }
   },
   mounted: function () {
-    if (process.env.NODE_ENV === 'development' && !this.$props.language) {
-      throw new Error(
-        `Prism component for language "${this.$props.language}" was not found, did you forget to register it? See all available ones: https://cdn.jsdelivr.net/npm/prismjs/components/`
-      );
-    }
     const code = document.createElement('code');
     code.innerHTML = Prism.highlight(this.$props.code, Prism.languages[this.$props.language], this.$props.language);
     this.$refs.code.appendChild(code);
