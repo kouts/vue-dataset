d10f46518170a6bc9a7c9879a89000738a44bc03
Kouts <koutsaftakis@gmail.com>
2021-07-11 23:27:02 +0300

    Replaced indexes computed with watcher

--- docs/.vuepress/components/Example2.vue
+++ docs/.vuepress/components/Example2.vue
@@ -82,7 +82,7 @@
           <dataset-show />
         </div>
         <div class="col-md-6">
-          <dataset-search ds-search-placeholder="Search..." :wait="300" />
+          <dataset-search ds-search-placeholder="Search..." />
         </div>
       </div>
       <div class="row">
