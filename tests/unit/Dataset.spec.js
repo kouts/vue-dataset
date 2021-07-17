ef5fe4a0b80af937c6b543430b60a8ed39f823b5
Kouts <koutsaftakis@gmail.com>
2021-07-17 08:53:02 +0300

    Added test for pagecount

--- tests/unit/Dataset.spec.js
+++ tests/unit/Dataset.spec.js
@@ -7,7 +7,7 @@ import { clone, waitNT } from '../../tests/utils.js'
 let wrapperComponent, wrapperDataset, wrapperTestComponent
 
 const TestComponent = {
-  inject: ['search', 'showEntries', 'setActive', 'datasetI18n', 'rdsPages', 'rdsPage'],
+  inject: ['search', 'showEntries', 'setActive', 'datasetI18n', 'rdsPages', 'rdsPage', 'rdsPagecount'],
   template: `
     <div>
       <div class="ds-pages">
@@ -15,7 +15,10 @@ const TestComponent = {
       </div>
       <div class="ds-page">
         {{ dsPage }}
-      </div>      
+      </div>
+      <div class="ds-pagecount">
+        {{ dsPagecount }}
+      </div>
     </div>
   `,
   computed: {
@@ -25,6 +28,9 @@ const TestComponent = {
     },
     dsPage() {
       return this.rdsPage()
+    },
+    dsPagecount() {
+      return this.rdsPagecount()
     }
   }
 }
@@ -65,18 +71,21 @@ describe('Dataset', () => {
   })
 
   it('passes the injected values and renders the slot', async () => {
-    const div = wrapperTestComponent.find('div')
-    expect(div.exists()).toBe(true)
-    const array = eval(div.find('.ds-pages').text())
+    const array = eval(wrapperTestComponent.find('.ds-pages').text())
     expect(array).toEqual([1, 2, 3, '...', 500])
   })
 
   it('resets the active page when data changes', async () => {
-    const newData = clone(users).slice(0, 200)
-    const div = wrapperTestComponent.find('div')
+    const newUsers = clone(users).slice(0, 200)
     wrapperDataset.vm.setActive(15)
     await waitNT(wrapperDataset.vm)
-    await wrapperComponent.setData({ users: newData })
-    expect(div.find('.ds-page').text()).toBe('1')
+    await wrapperComponent.setData({ users: newUsers })
+    expect(wrapperTestComponent.find('.ds-page').text()).toBe('1')
+  })
+
+  it('splits the into the corrent number of pages', async () => {
+    const newUsers = clone(users).slice(0, 301)
+    await wrapperComponent.setData({ users: newUsers })
+    expect(wrapperTestComponent.find('.ds-pagecount').text()).toBe('31')
   })
 })
