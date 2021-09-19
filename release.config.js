05c278d48c64453e288f7ad98d4fc6553b6eefed
Kouts <koutsaftakis@gmail.com>
2021-09-19 10:56:45 +0300

    ci: update release config

--- release.config.js
+++ release.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   branches: [
-    'main-disabled',
+    'master',
     {
       name: 'beta',
       prerelease: true
+    },
+    {
+      name: 'next',
+      channel: 'next'
     }
   ],
   plugins: [
@@ -15,18 +19,12 @@ module.exports = {
         changelogFile: 'CHANGELOG.md'
       }
     ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-        tarballDir: 'dist'
-      }
-    ],
+    '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'dist/**'],
+        assets: ['CHANGELOG.md'],
         // eslint-disable-next-line no-template-curly-in-string
         message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
