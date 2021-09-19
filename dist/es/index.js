d17de1e806f717300fa7907da65dcc8fe59142af
Kouts <koutsaftakis@gmail.com>
2021-09-19 10:30:51 +0300

    chore: clean dist folder

--- dist/es/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Dataset } from './Dataset.js';
-export { default as DatasetInfo } from './DatasetInfo.js';
-export { default as DatasetItem } from './DatasetItem.js';
-export { default as DatasetPager } from './DatasetPager.js';
-export { default as DatasetSearch } from './DatasetSearch.js';
-export { default as DatasetShow } from './DatasetShow.js';
-import './index-e93a2314.js';
-import './normalize-component-1efcb3aa.js';
-//# sourceMappingURL=index.js.map
