95c3d1f1e957c8bcd0fb11347a660b007b262db7
Kouts <koutsaftakis@gmail.com>
2021-09-25 13:47:03 +0300

    docs: added vuepress-plugin-vue-example package

--- vuepress-plugin-vue-example/clientAppEnhanceFiles.js
+++
@@ -1,6 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-import VueExample from './VueExample.vue'
-
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.component('VueExample', VueExample)
-})
