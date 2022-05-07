336f7d5976418b96fa7e50c1f9bbffca327c4009
kouts <koutsaftakis@gmail.com>
2022-05-07 23:19:32 +0300

    docs: fixed vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,22 @@
 const path = require('path')
 const VueExamplePlugin = require('vuepress-plugin-vue-example')
+const { defaultTheme } = require('vuepress-webpack')
+const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
 
 module.exports = {
   plugins: [
     VueExamplePlugin({
       componentsPath: '/docs/.vuepress/components/'
     }),
-    [
-      '@vuepress/docsearch',
-      {
-        apiKey: '1a6df20c3c6d9b729c4109ebce3e2eed',
-        indexName: 'next-vue-dataset'
-      }
-    ]
+    docsearchPlugin({
+      apiKey: '1a6df20c3c6d9b729c4109ebce3e2eed',
+      indexName: 'next-vue-dataset'
+    })
   ],
   dest: 'public',
   title: 'vue-dataset',
   description: 'A vue component to display datasets with filtering, paging and sorting capabilities!',
-  themeConfig: {
+  theme: defaultTheme({
     contributors: false,
     repo: 'https://github.com/kouts/vue-dataset/tree/next',
     darkMode: false,
@@ -49,7 +48,7 @@ module.exports = {
         ]
       }
     ]
-  },
+  }),
   alias: {
     '@': path.resolve(__dirname, '../../src'),
     '@playground': path.resolve(__dirname, '../../playground'),
