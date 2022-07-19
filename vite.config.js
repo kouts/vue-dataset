343ed0457d39df6b732f0e0dd5c6d467959717ea
kouts <koutsaftakis@gmail.com>
2022-07-19 12:40:52 +0300

    fix: fixed docs site

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  publicDir: 'public-vite',
   plugins: [
     vue(),
     createHtmlPlugin({
